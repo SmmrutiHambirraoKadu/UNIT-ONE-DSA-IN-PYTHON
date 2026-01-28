c31998c84f602a9bf47c2fddfaa0355e96357441
SmmrutiHambirraoKadu <smmrutihk@gmail.com>
2026-01-28 15:06:03 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,15 @@
+# 2.1 Create and access set elements
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7}
+
+print("Set1:", set1)
+print("Set2:", set2)
+
+# 2.2 Union of elements
+print("Union:", set1.union(set2))
+
+# 2.3 Intersection of elements
+print("Intersection:", set1.intersection(set2))
+
+# 2.4 Difference of elements
+print("Difference (Set1 - Set2):", set1.difference(set2))
