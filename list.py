a51dc217b876596aa62ec6d1ab33e2c1e642a586
SmmrutiHambirraoKadu <smmrutihk@gmail.com>
2026-01-28 15:05:42 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,20 @@
+# 1.1 Create and access list elements
+my_list = [10, 20, 30, 40, 50]
+print("Original List:", my_list)
+print("First element:", my_list[0])
+print("Last element:", my_list[-1])
+
+# 1.2 Add and remove list elements
+my_list.append(60)
+print("After append:", my_list)
+
+my_list.remove(30)
+print("After removing 30:", my_list)
+
+# 1.3 Sort list elements
+my_list.sort()
+print("Sorted list:", my_list)
+
+# 1.4 Reverse list elements
+my_list.reverse()
+print("Reversed list:", my_list)
