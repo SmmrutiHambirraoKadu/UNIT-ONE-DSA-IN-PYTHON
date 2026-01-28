b9695ed65061c45d98b6f2d58dd5fb89fdf42600
SmmrutiHambirraoKadu <smmrutihk@gmail.com>
2026-01-28 15:00:02 +0530

    Create tuple.py

---
+++ Create and access list elements/tuple.py
@@ -0,0 +1,17 @@
+my_tuple = (10, 20, 30, 40)
+print("Tuple:", my_tuple)
+print("Second element:", my_tuple[1])
+
+
+nested_tuple = (1, 2, (3, 4, 5))
+print("Nested Tuple:", nested_tuple)
+print("Access nested element:", nested_tuple[2][1])
+
+repeated_tuple = my_tuple * 2
+print("Repeated Tuple:", repeated_tuple)
+
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+concat_tuple = tuple1 + tuple2
+print("Concatenated Tuple:", concat_tuple)
