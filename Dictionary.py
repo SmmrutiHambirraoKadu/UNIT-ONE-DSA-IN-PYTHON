ccd16e4a471ed70667e7a44e2c3c00aed2669989
SmmrutiHambirraoKadu <smmrutihk@gmail.com>
2026-02-03 23:53:04 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -21,5 +21,5 @@ print("After removing 'course':", my_dict)
 dict1 = {"a": 1, "b": 2}
 dict2 = {"c": 3, "d": 4}
 
-merged_dict = dict1 | dict2   # Python 3.9+
+merged_dict = dict1 | dict2   
 print("Merged Dictionary:", merged_dict)
